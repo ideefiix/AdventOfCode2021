84a3f824af967576c49a3d17fc2e68e2dd001b32
ideefiix <flindu@gmail.com>
2021-12-08 11:08:32 +0100

    day 5 still

--- src/Day5.py
+++ src/Day5.py
@@ -1,9 +1,27 @@
 
+
+def add_overlap(points, vLine):
+    x_diff = abs(int(vLine[0])-int(vLine[2]))
+    y_diff = abs(int(vLine[1])-int(vLine[3]))
+
+    x_start = min(int(vLine[0]), int(vLine[2]))
+    y_start = min(int(vLine[1]), int(vLine[3]))
+
+    # Don't consider this case yet
+    if x_diff != 0 and y_diff != 0: return
+
+    for i in range(x_diff):
+        if str(x_start)
+
+
 if __name__ == '__main__':
     vent_list = []
-    playfield = []
+    points = {}
     with open('input5.txt', 'r') as file:
         for line in file.readlines():
             vent_line = line.strip().replace(' -> ', ',').split(',')
             vent_list.append(vent_line)
-            print(strr)
+            print(vent_list)
+        
+    for v in vent_list:
+        add_overlap(points, v)
