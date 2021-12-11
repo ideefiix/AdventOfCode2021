2a51984ff6749a13479529613ceafdcd41c61397
ideefiix <flindu@gmail.com>
2021-12-11 13:52:59 +0100

    Day 5 task 1 finished

--- src/Day5.py
+++ src/Day5.py
@@ -7,12 +7,52 @@ def add_overlap(points, vLine):
     x_start = min(int(vLine[0]), int(vLine[2]))
     y_start = min(int(vLine[1]), int(vLine[3]))
 
-    # Don't consider this case yet
-    if x_diff != 0 and y_diff != 0: return
+    # Diagonal Path
+    if x_diff != 0 and y_diff != 0: 
+        x_increases = vLine[2] > vLine[0]
+        y_increases = vLine[3] > vLine[1]
+        # Case 1:
+        # \
+        #  \
+        #   \
+        if x_increases == y_increases:
+            for i in range(x_diff + 1):
+                point = str(x_start + i) + "/" + str(y_start + i)
+                if point in points:
+                    points[point] = points[point] + 1
+                else:
+                    points.update({point: 1})
+        # Case 2
+        #   /
+        #  /
+        # /
+        if x_increases != y_increases:
+            y_max = max(int(vLine[1]), int(vLine[3]))
+            for i in range(x_diff + 1):
+                point = str(x_start + i) + "/" + str(y_max - i)
+                if point in points:
+                    points[point] = points[point] + 1
+                else:
+                    points.update({point: 1})
+        return
 
-    for i in range(x_diff):
-        if str(x_start)
+    #Horizontal Path
+    if x_diff != 0:
+        for i in range(x_diff + 1):
+            point = str(x_start + i) + "/" + str(y_start)
+            if point in points:
+                points[point] = points[point] + 1
+            else:
+                points.update({point: 1})
 
+    #Vertical Path
+    if y_diff != 0:
+        for j in range(y_diff + 1):
+            point = str(x_start) + "/" + str(y_start + j)
+            if point in points:
+                points[point] = points[point] + 1
+            else:
+                points.update({point: 1})
 
 if __name__ == '__main__':
     vent_list = []
@@ -21,7 +61,11 @@ if __name__ == '__main__':
         for line in file.readlines():
             vent_line = line.strip().replace(' -> ', ',').split(',')
             vent_list.append(vent_line)
-            print(vent_list)
-        
+    
+    #test_list = [['1', '4', '3', '4'], ['2','1','2','6'], ['1','1','4','4'], ['1', '5', '4', '2'],['4', '2', '1', '5']]
     for v in vent_list:
         add_overlap(points, v)
+    dangerPoints = [p for p in points if points[p] >= 2]
+    print(len(dangerPoints))
+
+
