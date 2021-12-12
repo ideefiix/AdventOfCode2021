a6ddcce0a7bfc769833766e63e2b234a13186475
ideefiix <flindu@gmail.com>
2021-12-12 12:24:11 +0100

    Day7 finished

--- src/Day7.py
+++ src/Day7.py
@@ -4,10 +4,19 @@ def fuel_cost(listt, position):
         cost += abs(position - crabPos)
     return cost
 
+def fuel_cost_task2(listt, position):
+    cost = 0
+    for crab_pos in listt:
+        diff = abs(position - crab_pos)
+        for i in range(diff + 1):
+            cost += i
+    return cost
+
 
 if __name__ == '__main__':
     crab_pos = []
     cost_dict = {}
+    cost_dict2 = {}
     with open('input7.txt', 'r') as file:
         lines = file.readlines()
         list_str = lines[0].strip().split(',')
@@ -16,8 +25,13 @@ if __name__ == '__main__':
 
     for position in range(max(crab_pos)):
         cost_dict.update({str(position): fuel_cost(crab_pos, position)})
+        cost_dict2.update({str(position): fuel_cost_task2(crab_pos, position)})
 
-    pos, cost = min(cost_dict.items(), key= lambda tup: tup[1])
+    pos1, cost1 = min(cost_dict.items(), key= lambda tup: tup[1])
+    pos2, cost2 = min(cost_dict2.items(), key= lambda tup: tup[1])
     
     print("Task 1")
-    print("Best position is " + str(pos) + " which has a cost of " + str(cost) )
+    print("Best position is " + str(pos1) + " which has a cost of " + str(cost1) )
+    print("--------------")
+    print("Task 2")
+    print("Best position is " + str(pos2) + " which has a cost of " + str(cost2) )
